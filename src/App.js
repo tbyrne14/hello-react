d2cb18fca799dcbeff442905a26113bfcec92e92
Tyler Byrne <byrnetyl@msu.edu>
2023-03-21 23:16:54 -0400

    test deploy commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Tyler
+          Learn React with Tyler and MSU!
         </a>
       </header>
     </div>
