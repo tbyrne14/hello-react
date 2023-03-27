b438174173dcf6abc34f2fb440a5cce316761b8a
Tyler Byrne <byrnetyl@msu.edu>
2023-03-27 19:16:33 -0400

    magic button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MagicButton() {
+  return (
+    <button>Magic</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +24,7 @@ function App() {
           Learn React with Tyler and MSU!
         </a>
       </header>
+      <MagicButton />
     </div>
   );
 }
